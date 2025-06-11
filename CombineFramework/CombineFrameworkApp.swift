e390305f20573a98c065a2f71a8212c1675bef52
Amit Sharma <amithastir@gmail.com>
2025-06-12 00:12:20 +0530

    Initial Commit

---
+++ CombineFramework/CombineFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFrameworkApp.swift
+//  CombineFramework
+//
+//  Created by Amit Sharma on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
